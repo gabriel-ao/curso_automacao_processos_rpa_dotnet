f329c32e71c5d6d87b5ee54bf86b706f6c40f513
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2024-09-04 00:12:16 -0300

    Finalizando aula 9 - automação com easy.Automation parte 1

---
+++ AutomationSeleniumChorme/RobotEasyAutomation/Checkout.cs
@@ -0,0 +1,41 @@
+﻿namespace RobotEasyAutomation
+{
+    public class Checkout
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Payment { get; set; }
+        public string NameCard { get; set; }
+        public string CreditCard { get; set; }
+        public string Expiration { get; set; }
+        public string Cvv { get; set; }
+
+
+        public static Checkout Map(string line)
+        {
+            var values = line.Split(',');
+
+            return new Checkout { 
+                FirstName = values[0], 
+                LastName = values[1],
+                Username = values[2],
+                Email = values[3],
+                Address = values[4],
+                Country = values[5],
+                State = values[6],
+                Zip = values[7],
+                Payment = values[8],
+                NameCard = values[9],
+                CreditCard = values[10],
+                Expiration = values[11],
+                Cvv = values[12]
+            };
+        }
+    }
+}
--- AutomationSeleniumChorme/RobotEasyAutomation/Program.cs
+++ AutomationSeleniumChorme/RobotEasyAutomation/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RobotEasyAutomation;
+
+try
+{
+    var filePath = "D:\\documentos\\github\\curso_automacao_processos_rpa_dotnet\\AutomationSeleniumChorme\\RobotEasyAutomation\\database_costumer.csv";
+
+    var lines = File.ReadAllLines(filePath);
+    //var checkouts = lines.Skip(1).Select(Checkout.Map).ToList();
+
+    var checkouts = lines.Skip(1)
+                         .Select(line => line.Split(',')) // Divide a linha em colunas
+                         .Where(parts => parts.Length >= 2) // Verifica se há pelo menos 2 colunas (ajuste conforme necessário)
+                         .Select(parts => new Checkout
+                         {
+                             FirstName = parts[0],
+                             LastName = parts[1],
+                             Username = parts[2],
+                             Email = parts[3],
+                             Address = parts[4],
+                             Country = parts[5],
+                             State = parts[6],
+                             Zip = parts[7],
+                             Payment = parts[8],
+                             NameCard = parts[9],
+                             CreditCard = parts[10],
+                             Expiration = parts[11],
+                             Cvv = parts[12]
+                         })
+                         .ToList();
+
+    foreach (var checkout in checkouts)
+    {
+        Console.WriteLine("checkout = " + checkout.FirstName);
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("messagem de erro:" + ex.Message);
+}
